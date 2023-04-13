56f5b95153a2ee1a13cc4f3ca6c26e4ed6a6940e
Randall Clark <randyclark88@gmail.com>
2023-04-13 07:39:36 -0500

    removed HelloWorld.java

--- src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import com.google.common.base.Joiner;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        String message = Joiner.on(" ").join(args.length > 0 ? args : new String[] {"World!"});
-        System.out.println("Hello, " + message);
-    }
-}
-
